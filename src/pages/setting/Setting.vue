0bfbb29429daf9455a9dd0e14e07548a8b6d1727
YQHY <2112562745@qq.com>
2024-11-18 21:51:41 +0800

    在设置界面加入导入书籍的按钮

--- src/pages/setting/Setting.vue
+++ src/pages/setting/Setting.vue
@@ -8,11 +8,89 @@
 
       <app-settings-theme />
       <!-- <app-settings-other /> -->
+      <v-btn
+        class="no-drag-area"
+        v-bind="$attrs"
+        variant="tonal"
+        color="secondary"
+        @click.prevent="fileInput.click()"
+      >
+        导入一本书
+      </v-btn>
+      <v-btn
+        class="no-drag-area"
+        v-bind="$attrs"
+        variant="tonal"
+        color="secondary"
+        @click.prevent="dirInput.click()"
+      >
+        导入许多书
+      </v-btn>
       <!-- <app-settings-reset /> -->
+      <input
+        ref="dirInput"
+        type="file"
+        @change="handleDirChange"
+        class="hidden"
+        webkitdirectory
+      />
+      <input
+        ref="fileInput"
+        type="file"
+        @change="handleFileChange"
+        class="hidden"
+      />
     </div>
   </section>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import AppSettingsTheme from "./components/Theme.vue";
+const fileInput = ref(null);
+const dirInput = ref(null);
+import EPub from "epubjs";
+import localForage from "localforage";
+localForage.config({
+  name: "epubBooks",
+});
+
+function handleDirChange(event) {
+  let files = Array.from(event.target.files);
+  files.forEach((file) => {
+    const reader = new FileReader();
+    reader.onload = async (e) => {
+      const book = EPub(e.target.result);
+      let cover = await book.loaded.cover;
+      let coverBlob = await book.archive.getBlob(cover);
+      const coverUrl = URL.createObjectURL(coverBlob);
+      let metadata = await book.loaded.metadata;
+      let bookName = metadata.title.replace(/\(.*?\) |（.*?）/g, "");
+      await localForage.setItem(bookName, {
+        book: e.target.result,
+        name: bookName,
+        cover: coverBlob,
+      });
+    };
+    reader.readAsArrayBuffer(file);
+  });
+}
+
+function handleFileChange(event) {
+  let file = event.target.files[0];
+  const reader = new FileReader();
+  reader.onload = async (e) => {
+    const book = EPub(e.target.result);
+    // 获取封面
+    const cover = await book.loaded.cover;
+    const coverBlob = await book.archive.getBlob(cover);
+    const metadata = await book.loaded.metadata;
+    const bookName = metadata.title.replace(/\(.*?\) |（.*?）/g, "");
+    await localForage.setItem(bookName, {
+      book: e.target.result,
+      name: bookName,
+      cover: coverBlob,
+    });
+  };
+  reader.readAsArrayBuffer(file);
+}
 </script>
